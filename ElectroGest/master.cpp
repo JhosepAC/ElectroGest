e7075d2acb34b83adba6f1b7784cef2af637f831
Jhosep Jamil Argomedo Camacho <140860808+JosiFat@users.noreply.github.com>
2024-05-14 10:17:58 -0500

    Línea de prueba

--- ElectroGest/master.cpp
+++ ElectroGest/master.cpp
@@ -5,6 +5,8 @@ int main() {
     // Configurar la localización para que los caracteres especiales se muestren correctamente
     setlocale(LC_ALL, "");
 
+    // Comentario de prueba
+
     // Menú principal
     generalMenu();
 
