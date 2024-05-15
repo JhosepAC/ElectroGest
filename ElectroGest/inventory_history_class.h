d6973bfd22c84b14e59e203ac88ced85d0a986fd
Jhosep Jamil Argomedo Camacho <140860808+JosiFat@users.noreply.github.com>
2024-05-15 10:53:46 -0500

    Clase de Historial de Inventario modificada (Comentarios)

--- ElectroGest/inventory_history_class.h
+++ ElectroGest/inventory_history_class.h
@@ -3,10 +3,12 @@
 
 class HISTORIAL_INVENTARIO {
 public:
+    // Atributos
     string codigoProducto;
     int cantidad;
     bool esEntrada; // Indica si es una entrada (true) o una salida (false)
 
-    HISTORIAL_INVENTARIO(const std::string& codigo, int cantidad, bool esEntrada)
+    // Constructor
+    HISTORIAL_INVENTARIO(const string& codigo, int cantidad, bool esEntrada)
         : codigoProducto(codigo), cantidad(cantidad), esEntrada(esEntrada) {}
 };
