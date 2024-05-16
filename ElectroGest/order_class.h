2d18c82781a795187c8a967be7dbddf988af7e70
Jhosep Jamil Argomedo Camacho <140860808+JosiFat@users.noreply.github.com>
2024-05-16 10:55:20 -0500

    Clase de Pedido modificada (Comentarios)

--- ElectroGest/order_class.h
+++ ElectroGest/order_class.h
@@ -1,22 +1,18 @@
 #pragma once
 #include <iostream>
 
-// Clase para representar un pedido
 class PEDIDO {
 private:
-    std::string codigoProducto;
+    string codigoProducto;
     int cantidad;
 
 public:
-    PEDIDO(const std::string& codigo, int cantidad) : codigoProducto(codigo), cantidad(cantidad) {}
+    // Constructor de la clase
+    PEDIDO(const string& codigo, int cantidad) : codigoProducto(codigo), cantidad(cantidad) {}
 
     // Método para obtener el código del producto
-    std::string getCodigoProducto() const {
-        return codigoProducto;
-    }
+    string getCodigoProducto() const { return codigoProducto; }
 
     // Método para obtener la cantidad del producto
-    int getCantidad() const {
-        return cantidad;
-    }
+    int getCantidad() const { return cantidad; }
 };
