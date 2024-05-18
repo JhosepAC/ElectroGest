e21716f6137e309fdd8f117b73783865eb5a3ea9
Jhosep Jamil Argomedo Camacho <140860808+JosiFat@users.noreply.github.com>
2024-05-18 14:36:24 -0500

    Nombre agregado a la consola

--- ElectroGest/master.cpp
+++ ElectroGest/master.cpp
@@ -5,6 +5,8 @@ int main() {
     // Configurar la localización para que los caracteres especiales se muestren correctamente
     setlocale(LC_ALL, "");
 
+    system("title ElectroGest");
+
     // Menú principal
     generalMenu();
 
